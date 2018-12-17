346638e694b1780aa29f9ed9a6a67d3d2b7c1526
Daniel Ling <39923191+dongyaoling@users.noreply.github.com>
2018-12-16 22:35:07 -0800

    Update word_search.java

--- Word Search II/word_search.java
+++ Word Search II/word_search.java
@@ -1,8 +1,9 @@
-/*Given a 2D board and a list of words from the dictionary, find all words in the board.
-
+/* Given a 2D board and a list of words from the dictionary, 
+find all words in the board.
 Each word must be constructed from letters of sequentially adjacent cell,
 where "adjacent" cells are those horizontally or vertically neighboring.
 The same letter cell may not be used more than once in a word.*/
+
 class Solution {
     public List<String> findWords(char[][] board, String[] words) {
         Trie map = new Trie();

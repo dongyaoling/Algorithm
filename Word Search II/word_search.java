e3c852f48b6a873bad8ebd8de6ff9e04fa9ac132
Daniel Ling <39923191+dongyaoling@users.noreply.github.com>
2018-12-16 22:32:03 -0800

    Add files via upload

---
+++ Word Search II/word_search.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<String> findWords(char[][] board, String[] words) {
+        Trie map = new Trie();
+        for (String w : words){
+            Trie p = map;
+            for (char c : w.toCharArray()){
+                if (p.next[c - 'a'] == null)
+                    p.next[c - 'a'] = new Trie();
+                p = p.next[c - 'a'];
+            }
+            p.word = w;
+        }
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < board.length; i ++){
+            for (int j = 0; j < board[0].length; j ++)
+                check(board, i, j, res, map);
+        }
+        return res;
+    }
+    
+    private void check(char[][] board, int i, int j, List<String> res, Trie map){
+        if (i < 0 || j < 0 || i > board.length - 1 || j > board[0].length - 1)
+            return;
+        char c = board[i][j];
+        if (c == '#' || map.next[c - 'a'] == null)
+            return;
+        map = map.next[c - 'a'];
+        if (map.word != null){
+            res.add(map.word);
+            map.word = null;
+        }
+        board[i][j] = '#';
+        check(board, i + 1, j, res, map);
+        check(board, i - 1, j, res, map);
+        check(board, i, j + 1, res, map);
+        check(board, i, j - 1, res, map);
+        board[i][j] = c;
+    }
+}
+
+class Trie {
+    Trie[] next = new Trie[26];
+    String word;
+}
